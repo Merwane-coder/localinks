ddbb8341e973c27d1b36d88b6cf4e6d001421871
franck-tchassi <franck.tchassi@laplateforme.io>
2025-07-30 23:34:44 +0200

    ajout fichier generate

--- src/app/api/stripe-webhook.ts
+++ src/app/api/stripe-webhook.ts
@@ -5,7 +5,7 @@ import {prisma} from '../../db/prisma';
 import { Readable } from 'stream';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-06-30.basil',
+  apiVersion: '2025-07-30.basil',
 });
 
 
